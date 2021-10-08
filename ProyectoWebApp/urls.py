1915eac407943e72a718fafc6cc65ce73f456912
Santiago Jimenez <Santiago0726258@gmail.com>
2021-10-08 14:14:35 -0500

    Ventanas(Jerarquia de plantillas)

--- ProyectoWebApp/urls.py
+++ ProyectoWebApp/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('servicios',views.servicios, name="Servicios"),
     path('tienda',views.tienda, name="Tienda"),
     path('blog',views.blog, name="Blog"),
-    path('contacto',views.home, name="Contacto"),
+    path('contacto',views.contacto, name="Contacto"),
 ]
